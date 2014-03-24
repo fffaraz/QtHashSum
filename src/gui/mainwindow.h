3bf329fc5c035c7fa93bf8402c7175ff34acc3ed
Faraz Fallahi <fffaraz@gmail.com>
2014-03-24 15:33:41 +0430

    GUI

--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "mycryptographichash.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    MyCryptographicHash *chash;
 };
 
 #endif // MAINWINDOW_H
