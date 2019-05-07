ea063fb46e97c2def097d29d79c83dec01059cf3
Faraz Fallahi <fffaraz@gmail.com>
2019-05-06 23:08:41 -0500

    Update mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -146,6 +146,8 @@ void MainWindow::on_btnResticBackup_clicked()
 {
     // --exclude .cache --exclude .local
     // mysqldump database | restic backup --stdin --stdin-filename database.sql
+    // mysqldump --databases database_name -u database_user -p | restic backup --stdin --stdin-filename database_dump.sql
+    // mysqldump -u database_user -p --all-databases | restic backup --stdin --stdin-filename all_databases.sql
     QString backup = ui->txtResticBackup->text();
     if(backup.size() < 1) return;
     ResticDialog *rd = new ResticDialog(ui->txtRestic->text(), "--verbose --verbose backup " + backup, getResticEnv(), this);
@@ -162,12 +164,15 @@ void MainWindow::on_btnResticCheck_clicked()
 
 void MainWindow::on_btnResticSnapshots_clicked()
 {
+    // restic diff $SnapshotID1 $SnapshotID2
     ResticDialog *rd = new ResticDialog(ui->txtRestic->text(), "--verbose --verbose snapshots", getResticEnv(), this);
     rd->show();
 }
 
 void MainWindow::on_btnResticRestore_clicked()
 {
+    // restic restore $SnapshotID --target $LocationToRestoreTo --path $PathBeingRestored
+    // restic restore $SnapshotID --target $LocationToRestoreTo --include $PathtoFileBeingRestored
     QString restore = ui->txtResticRestore->text();
     if(restore.size() < 1) return;
     QString snapshot = ui->txtResticSnapshot->text();
